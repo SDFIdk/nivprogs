4ac8ce87d0e90de5d3af4071e2488704faa4191c
Simon Kokkendorff <silyko@gmail.com>
2014-04-23 11:43:38 +0200

    dist changes

--- DistSource.py
+++ DistSource.py
@@ -1,7 +1,8 @@
 #Make a source distribution of the "niv-prog suite"
 import sys,os,time,shutil,glob
 SUITE="NivProgs"
-SCRIPTS=["VisFix.py","VisFix_main.py","MGL.py","MTL.py","ImportTest.py"]
+WSCRIPTS=["VisFix.py","VisFix_main.py","MGL.py","MTL.py","ImportTest.py"] #gui apps
+SCRIPTS=["SummaRho.py"]  #console scripts
 VER_INFO=os.path.join(SUITE,"version.txt")
 if os.path.exists(SUITE):
 	shutil.rmtree(SUITE)
@@ -11,8 +12,10 @@ for name in module_files:
 	shutil.copy(name,os.path.join(SUITE,"MyModules",os.path.basename(name)))
 #shutil.copytree("MyModules",os.path.join(SUITE,"MyModules"))
 shutil.copytree("mcontent",os.path.join(SUITE,"mcontent"))
+for script in WSCRIPTS:
+	shutil.copy(script,os.path.join(SUITE,os.path.splitext(os.path.basename(script))[0]+".pyw"))
 for script in SCRIPTS:
-	shutil.copy(script,os.path.join(SUITE,os.path.splitext(script)[0]+".pyw"))
+	shutil.copy(script,os.path.join(SUITE,os.path.basename(script)))
 os.system("hg identify > %s" %(VER_INFO))
 f=open(VER_INFO,"a")
 f.write("\nGenerated at %s\n" %time.asctime())
